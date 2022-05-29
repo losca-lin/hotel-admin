5a590b46b6b940d0cabecb17fd3c1c75b7979bdc
losca <783840358@qq.com>
2022-05-29 23:30:17 +0800

    基于mq实现elk数据的一致性

---
+++ src/main/java/cn/itcast/hotel/config/SystemConstants.java
@@ -0,0 +1,28 @@
+package cn.itcast.hotel.config;
+
+/**
+ * @author Losca
+ * @date 2022/5/29 20:34
+ */
+public class SystemConstants {
+    /**
+     * 交换机
+     */
+    public final static String HOTEL_EXCHANGE = "hotel.topic";
+    /**
+     * 监听新增和修改的队列
+     */
+    public final static String HOTEL_INSERT_QUEUE = "hotel.insert.queue";
+    /**
+     * 监听删除的队列
+     */
+    public final static String HOTEL_DELETE_QUEUE = "hotel.delete.queue";
+    /**
+     * 新增或修改的RoutingKey
+     */
+    public final static String HOTEL_INSERT_KEY = "hotel.insert";
+    /**
+     * 删除的RoutingKey
+     */
+    public final static String HOTEL_DELETE_KEY = "hotel.delete";
+}
